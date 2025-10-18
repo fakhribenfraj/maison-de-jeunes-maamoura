1fb26acb74b483f6be4b13fb7d14d434c3a3d198
fakhreddine.benfraj <fakhreddine.benfraj@jiway.lu>
2025-10-18 12:28:02 +0100

    used flex

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-4xl font-bold text-center text-primary">
-        Maison de Jeunes Maamoura
-      </h1>
-      <h3 className="text-2xl font-bold text-center text-secondary">
-        meilleur espace pour ton enfant
-      </h3>
+    <div className="font-sans flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-bold text-center text-primary">
+          Maison de Jeunes Maamoura
+        </h1>
+        <h3 className="text-2xl font-bold text-center text-secondary">
+          meilleur espace pour ton enfant
+        </h3>
+      </div>
 
       <p className="text-center text-lg max-w-xl">
         Bienvenue à la Maison de Jeunes Maamoura! Nous sommes dédiés à offrir un
