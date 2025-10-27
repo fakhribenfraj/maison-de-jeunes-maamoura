2cac2dd3a1faf2c86bc61e7ad27a8569a46e3d20
ahmed757dbs-cloud <ahmed757dbs@gmail.com>
2025-10-27 10:07:41 +0100

    the rest 3 porograms components

--- src/components/sections/programs/ProgramsSection.jsx
+++ src/components/sections/programs/ProgramsSection.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Robotique from "./Robotique";
+import Arts from "./Arts";
+import Sports from "./Sports";
+import Painting from "./Painting";
 
 const ProgramsSection = () => {
-  return <section>
-    <Robotique/>
-  </section>;
-
+  return (
+    <section>
+      <div className="flex justify-center">
+        <Robotique />
+        <Arts />
+        <Sports />
+        <Painting />
+      </div>
+    </section>
+  );
 };
 
 export default ProgramsSection;
+   
--- src/components/sections/programs/Robotique.jsx
+++ src/components/sections/programs/Robotique.jsx
@@ -1,9 +1,9 @@
 export default function Robotique () {
  return(
-    <div className="rounded-lg w-52 h-52">
-        <h1 className="text-blue-800">Robotique</h1>
-        <p>by fakhri</p>
-        <div className="p-2 bg-blue-700 text-white rounded-md flex justify-center items-center">learn more</div>
+    <div className="rounded-lg w-45 h-40 bg-blue-50 mt-4 mx-3">
+        <h1 className="text-blue-800 text-xl flex justify-center">Robotique</h1>
+        <p className="ml-3 mt-1">by fakhri</p>
+        <div className="my-6 p-2 bg-blue-700 text-white rounded-3xl flex justify-center items-center mx-6">learn more</div>
     </div>
  )
 }
