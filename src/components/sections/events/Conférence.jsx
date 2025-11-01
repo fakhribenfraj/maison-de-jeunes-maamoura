d059417bf816d220205cff14a2d15d2697115578
ahmed757dbs-cloud <ahmed757dbs@gmail.com>
2025-11-01 14:26:52 +0100

    .

--- src/components/sections/events/Conférence.jsx
+++ src/components/sections/events/Conférence.jsx
@@ -1,6 +1,6 @@
 export default function Conférence() {
   return (
-    <div className="bg-white rounded-2xl shadow p-4 flex items-center justify-between mb-4 w-90 h-20 mb-4">
+    <div className="bg-white rounded-2xl shadow p-4 flex items-center justify-between w-90 h-20 mb-4">
       <div className="flex items-center gap-4">
         <div className="bg-yellow-400 text-white text-center rounded-xl w-16 h-16 flex flex-col justify-center">
           <div className="text-2xl text-bold">20</div>
