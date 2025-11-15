bc68ef1d44401565b1bb44135c520b962458d776
ahmed757dbs-cloud <ahmed757dbs@gmail.com>
2025-11-15 16:13:34 +0100

    header respansev

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -3,14 +3,14 @@ import Maison_de_Jeune from "./Maison_de_Jeunes";
 
 const Header = () => {
   return (
-    <div className="flex justify-center gap-4 mt-1.5 bg-[#FFF8E9] py-16">
-      <Maison_de_Jeune flex="flex justify-center gap-1" />
+    <div className="hidden md:flex md:justify-center md:gap-4 md:mt-1.5 md:bg-[#FFF8E9] md:py-16">
+      <Maison_de_Jeune flex="md:flex md:justify-center md:gap-1" />
 
-      <h1 className="text-indigo-900 mt-5">Programs</h1>
-      <h1 className="text-indigo-900 mt-5">Events</h1>
-      <h1 className="text-indigo-900 mt-5">Galerei</h1>
-      <h1 className="text-indigo-900 mt-5">About</h1>
-      <h1 className="bg-blue-900 text-white mt-2.5 p-3 rounded-xl w-21">
+      <h1 className="hidden md:block md:text-indigo-900 md:mt-5">Programs</h1>
+      <h1 className="hidden md:block md:text-indigo-900 md:mt-5">Events</h1>
+      <h1 className="hidden md:block md:text-indigo-900 md:mt-5">Galerei</h1>
+      <h1 className="hidden md:block md:text-indigo-900 md:mt-5">About</h1>
+      <h1 className="hidden md:block md:bg-blue-900 md:text-white md:mt-2.5 md:p-3 md:rounded-xl md:w-21">
         Contact
       </h1>
     </div>
