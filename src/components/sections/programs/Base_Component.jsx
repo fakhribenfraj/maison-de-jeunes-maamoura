f09c0fc99927df3a840cd8c1963e2495903548a6
ahmed757dbs-cloud <ahmed757dbs@gmail.com>
2025-12-06 15:36:49 +0100

    modification for the programssection

--- src/components/sections/programs/Base_Component.jsx
+++ src/components/sections/programs/Base_Component.jsx
@@ -6,12 +6,12 @@ export default function Base_Component({
   botten_color,
 }) {
   return (
-    <div className={"rounded-lg w-45 h-40 mt-4 mx-3 " + color}>
+    <div className={"rounded-lg w-50 h-45 mt-4 mx-3 shadow-xl " + color}>
       <h1 className={"text-xl flex justify-center " + title_color}>{title}</h1>
       <p className="ml-3 mt-1">{teacher}</p>
       <div
         className={
-          "my-6 p-2 text-white rounded-3xl flex justify-center items-center mx-6 " +
+          "my-6 p-2 text-white rounded-3xl flex justify-center items-center mx-6 mt10 " +
           botten_color
         }
       >
