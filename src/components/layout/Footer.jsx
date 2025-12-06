8aaa31381be988ead2e6e8ddac75e13c0feb15af
ahmed757dbs-cloud <ahmed757dbs@gmail.com>
2025-12-06 14:37:41 +0100

    footer responsev

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ import Maison_de_Jeune from "./header/Maison_de_Jeunes";
 const Footer = () => {
   return (
     <div className="flex flex-col">
-      <section className="flex gap-20 justify-between">
+      <section className="flex flex-col md:flex md:flex-row md:gap-20 ml-7 gap-4 md:items-center md:justify-between">
         <div>
           <Maison_de_Jeune flex="flex gap-1" />
           <h3>123 Rue de Jeunesse</h3>
@@ -20,7 +20,7 @@ const Footer = () => {
         </div>
         <div className="flex justify-center flex-col items-center">
           <h3>Follow us</h3>
-          <span className="flex justify-end gap-4">
+          <span className="flex md:justify-end md:gap-4">
             <img src="/images/facebook-logo.png" alt=" " className="w-6 h-6" />
             <img src="/images/linkin-logo.png" alt=" " className="w-6 h-6" />
           </span>
